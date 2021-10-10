f722b7c7364eb699287cce0db2bb8f7fbc88c687
BrideL <biv.2097@gmail.com>
2021-10-10 13:53:32 +0300

    fix: addd version of settings file
    This will make settings files compatible in the future

--- Wallomizer/Settings.cpp
+++ Wallomizer/Settings.cpp
@@ -13,6 +13,7 @@ bool Settings::bRunSlideshow = true;
 
 namespace Settings
 {
+	constexpr unsigned char settingsVersion = 1;
 	bool bAbortDelay = false;
 	bool bReplayDelay = false;
 	unsigned long delayed = 0;
@@ -27,6 +28,7 @@ void Settings::saveSettings()
 	_wfopen_s(&pFile, path, L"wb");
 	if (pFile != NULL)
 	{
+		fwrite(&settingsVersion, sizeof(settingsVersion), 1, pFile);
 		fwrite(&loadOnStartup, sizeof(loadOnStartup), 1, pFile);
 		fwrite(&delay, sizeof(delay), 1, pFile);
 		fwrite(&username, sizeof(username), 1, pFile);
@@ -46,6 +48,14 @@ void Settings::loadSettings()
 	_wfopen_s(&pFile, path, L"rb");
 	if (pFile != NULL)
 	{
+		unsigned char fileVersion = 0;
+		fwrite(&fileVersion, sizeof(fileVersion), 1, pFile);
+		if (fileVersion != settingsVersion)
+		{
+			fclose(pFile);
+			saveSettings();
+			return;
+		}
 		fread(&loadOnStartup, sizeof(loadOnStartup), 1, pFile);
 		fread(&delay, sizeof(delay), 1, pFile);
 		fread(&username, sizeof(username), 1, pFile);
